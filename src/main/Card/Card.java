4307aeeac6fc7f9868d26317f9145e6c45f80d76
Clem <duval.sch@gmail.com>
2021-03-17 16:34:02 +0100

    Eclipse compat

--- src/main/Card/Card.java
+++ src/main/Card/Card.java
@@ -4,9 +4,8 @@ public class Card {
     private int EnergyCost;
     private String name;
 
-    public Card(int energyCost, String name, String type, Effect effect) {
+    public Card(int energyCost, String name, String type) {
         EnergyCost = energyCost;
         this.name = name;
-        this.effect = effect;
     }
 }
