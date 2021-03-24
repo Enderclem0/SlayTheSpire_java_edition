892e00e61981e6c14c0c56a92109ea604b4392e3
Enderclem0 <cduval07@etud.u-pem.fr>
2021-03-24 16:43:56 +0100

    Player

--- src/main/Player.java
+++ src/main/Player.java
@@ -1,15 +1,35 @@
 package main;
 
+import main.Card.CardPile;
+
 public class Player {
     private int hp;
     private int energy;
     private int hpMax;
-    private  int energyMax;
-
+    private int energyMax;
+    private int gold;
+    private int currentStage=0;
+    private CardPile Deck = new CardPile();
     public Player(int hp, int energy, int hpMax, int energyMax) {
         this.hp = hp;
         this.energy = energy;
         this.hpMax = hpMax;
         this.energyMax = energyMax;
+    }
+    public void damageHp(int dmg){
+        hp-=dmg;
+        if (hp<=0){
+            UI ui=UI.getUI();
+            ui.gameOver();
+        }
+    }
+    public void healHp(int heal){
+        hp+=heal;
+        if (hp>hpMax){
+            hp=hpMax;
+        }
+    }
+    public void progress(){
+        currentStage++;
     }
 }
