a0e89891357e5c84be04af9a4cd4d8aa5ab9bf10
Amalaric <lamalaric93@gmail.com>
2021-03-24 16:09:00 +0100

    Player constructor

--- src/main/Player.java
+++ src/main/Player.java
@@ -1,4 +1,15 @@
 package main;
 
 public class Player {
+    private int hp;
+    private int energy;
+    private int hpMax;
+    private  int energyMax;
+
+    public Player(int hp, int energy, int hpMax, int energyMax) {
+        this.hp = hp;
+        this.energy = energy;
+        this.hpMax = hpMax;
+        this.energyMax = energyMax;
+    }
 }
